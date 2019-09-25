bd29ed7369a3df5c31236693bd877f563aa43124
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-25 21:30:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,22 @@
 class Dog
-  attr_accessor :name, :owner
+  attr_accessor :owner, :mood
+  attr_reader :name
+
   @@all = []
 
-  def initialize(name, owner)
+  def initialize(name, owner, mood = 'nervous')
     @name = name
     @owner = owner
+    @mood = mood
+    @owner.add_dog(self)
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
